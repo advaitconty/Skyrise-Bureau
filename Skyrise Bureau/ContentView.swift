dc7220fcae55a7ba6af79b325931fed68cba0555
contyadvait <advaitthepro@outlook.com>
2025-11-05 10:51:11 +0800

    Initial Commit

---
+++ Skyrise Bureau/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skyrise Bureau
+//
+//  Created by Milind Contractor on 5/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skyrise Bureau/Skyrise_BureauApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skyrise_BureauApp.swift
+//  Skyrise Bureau
+//
+//  Created by Milind Contractor on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Skyrise_BureauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
