0dce0b76f393098886476325a7a2186474019729
contyadvait <advaitthepro@outlook.com>
2025-11-11 18:12:53 +0800

    Added simple animation for the welcome screen, started on that

--- Skyrise Bureau/WelcomeView.swift
+++ Skyrise Bureau/WelcomeView.swift
@@ -8,35 +8,70 @@
 import SwiftUI
 
 struct WelcomeView: View {
-    @State var showWelcomeText: Bool = true
-    @State var showLogoIcon: Bool = true
+    @State var showLogo: Bool = true
+    @State var showBody: Bool = false
+    @State var error: Bool = false
+    @State var errorText: String = ""
+    @State var newAirlineName: String = ""
+    
+    
     var body: some View {
         VStack {
-            HStack {
-                if showLogoIcon {
+            if showLogo {
+                HStack {
                     Image(systemName: "airplane")
-                        .transition(.move(edge: .trailing))
+                        .font(.title)
+                    Text("Welcome to Skyrise Bureau!    ")
+                        .font(.title)
+                        .fontWidth(.expanded)
                         .onAppear {
                             DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
                                 withAnimation(.default, completionCriteria: .logicallyComplete) {
-                                    showWelcomeText = false
+                                    showLogo = false
                                 } completion: {
-                                    withAnimation(completionCriteria: .removed) {
-                                        showLogoIcon = false
-                                    } completion: {
-                                        print("Done")
+                                    withAnimation {
+                                        showBody = true
                                     }
                                 }
-                                
                             }
                         }
                 }
-                if showWelcomeText {
-                    Text("Welcome to Skyrise Bureau!")
-                        .fontWidth(.expanded)
-                }
+                .transition(.push(from: .leading))
             }
-         }
+            if showBody {
+                VStack {
+                    Text("Select an airline name to get started!")
+                        .font(.title3)
+                        .fontWidth(.expanded)
+                    
+                    HStack {
+                        Text("Airline name")
+                            .fontWidth(.expanded)
+                        Spacer()
+                        TextField("Skyward Collective", text: $newAirlineName)
+                            .textFieldStyle(.roundedBorder)
+                            .monospaced()
+                    }
+                    
+                    HStack {
+                        Text("IATA Code")
+                            .fontWidth(.expanded)
+                        Spacer()
+                        TextField("2-letter airline code (e.g., AA, BA, SQ)", text: $newAirlineName)
+                            .textFieldStyle(.roundedBorder)
+                            .monospaced()
+                            .onChange(of: newAirlineName) { oldValue, newValue in
+                                if newValue.count > 2 {
+                                    newAirlineName = String(newValue.prefix(2))
+                                }
+                            }
+                    }
+                }
+                .transition(.blurReplace)
+            }
+        }
+        .padding()
+        .frame(minWidth: 500, minHeight: 400)
     }
 }
 
