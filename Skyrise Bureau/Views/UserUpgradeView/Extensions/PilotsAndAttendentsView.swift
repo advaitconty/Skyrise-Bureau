100a6c7ffa273117dcb9533c609e1cc7a2c2f160
contyadvait <advaitthepro@outlook.com>
2025-11-20 21:55:19 +0800

    commit

--- Skyrise Bureau/Views/UserUpgradeView/Extensions/PilotsAndAttendentsView.swift
+++ Skyrise Bureau/Views/UserUpgradeView/Extensions/PilotsAndAttendentsView.swift
@@ -21,10 +21,19 @@ extension UserUpgradeView {
                     .font(.system(size: 36))
                 Text("$\(userData.weeklyPilotSalary.withCommas)/week")
                     .fontWidth(.condensed)
+                Text("\(String(Int(userData.pilotHappiness * 100)))%")
+                    .fontWidth(.condensed)
                 Spacer()
                 HStack {
                     Button {
-                        
+                        withAnimation {
+                            userData.weeklyPilotSalary = userData.weeklyPilotSalary + 50
+                            var adder: Double = Double.random(in: 0.01...0.05)
+                            while adder + userData.pilotHappiness > 1 {
+                                adder = Double.random(in: 0.01...0.05)
+                            }
+                            userData.pilotHappiness = userData.pilotHappiness + Double.random(in: 0.01...0.05)
+                        }
                     } label: {
                         Spacer()
                         Image(systemName: "arrowtriangle.up")
@@ -46,7 +55,7 @@ extension UserUpgradeView {
 
             }
             .padding(5)
-            .frame(width: 150, height: 125)
+            .frame(width: 150, height: 140)
             .background(colorScheme == .dark ? .white.opacity(0.1) : .black.opacity(0.1))
             .clipShape(RoundedRectangle(cornerRadius: 10.0))
             
@@ -60,6 +69,8 @@ extension UserUpgradeView {
                     .font(.system(size: 36))
                 Text("$\(userData.weeklyFlightAttendentSalary.withCommas)/week")
                     .fontWidth(.condensed)
+                Text("\(String(Int(userData.flightAttendentHappiness * 100)))%")
+                    .fontWidth(.condensed)
                 Spacer()
                 HStack {
                     Button {
@@ -85,7 +96,7 @@ extension UserUpgradeView {
 
             }
             .padding(5)
-            .frame(width: 150, height: 125)
+            .frame(width: 150, height: 140)
             .background(colorScheme == .dark ? .white.opacity(0.1) : .black.opacity(0.1))
             .clipShape(RoundedRectangle(cornerRadius: 10.0))
             VStack {
@@ -97,6 +108,8 @@ extension UserUpgradeView {
                 Image(systemName: "wrench.and.screwdriver")
                     .font(.system(size: 36))
                 Text("$\(userData.weeklyFlightMaintainanceCrewSalary.withCommas)/week")
+                    .fontWidth(.condensed)
+                Text("\(String(Int(userData.maintainanceCrewHappiness * 100)))%")
                     .fontWidth(.condensed)
                 Spacer()
                 HStack {
@@ -122,7 +135,7 @@ extension UserUpgradeView {
                 }
             }
             .padding(5)
-            .frame(width: 150, height: 125)
+            .frame(width: 150, height: 140)
             .background(colorScheme == .dark ? .white.opacity(0.1) : .black.opacity(0.1))
             .clipShape(RoundedRectangle(cornerRadius: 10.0))
         }
