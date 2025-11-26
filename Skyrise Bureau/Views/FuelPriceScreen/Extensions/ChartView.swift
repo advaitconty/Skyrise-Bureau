cfdc15b2b08c8100693ee20895699101923e8fa8
contyadvait <advaitthepro@outlook.com>
2025-11-26 13:12:38 +0800

    feat: finished fuel view picker

---
+++ Skyrise Bureau/Views/FuelPriceScreen/Extensions/ChartView.swift
@@ -0,0 +1,56 @@
+//
+//  ChartView.swift
+//  Skyrise Bureau
+//
+//  Created by Milind Contractor on 26/11/25.
+//
+
+import SwiftUI
+import Charts
+
+extension FuelPriceView {
+    func chartView() -> some View {
+        VStack {
+            HStack {
+                Text("Price over last 20 hours")
+                    .font(.caption)
+                    .fontWidth(.expanded)
+                Spacer()
+            }
+            Chart(lastFewFuelPriceItem, id: \.id) { price in
+                LineMark(x: .value("Fuel", price.hour), y: .value("Fuel Price", price.fuelPrice))
+                    .interpolationMethod(.monotone)
+            }
+            HStack {
+                if let maxItem = lastFewFuelPriceItem.max(by: { a, b in
+                    a.fuelPrice < b.fuelPrice
+                }) {
+                    Text("MAX:\n")
+                        .font(.caption)
+                        .fontWidth(.condensed)
+                    +
+                    Text("$\(String(format: "%.2f", maxItem.fuelPrice))")
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .fontWidth(.expanded)
+                }
+                Spacer()
+                if let minItem = lastFewFuelPriceItem.max(by: { a, b in
+                    a.fuelPrice > b.fuelPrice
+                }) {
+                    Text("MIN:\n")
+                        .font(.caption)
+                        .fontWidth(.condensed)
+                    +
+                    Text("$\(String(format: "%.2f", minItem.fuelPrice))")
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .fontWidth(.expanded)
+                }
+                Spacer()
+                /// TODO: Add volatility
+
+            }
+        }
+    }
+}
